d3086eb9e250437cfedcd9d70a0372cc4976d8f6
Quenntinne <wilbauxq@gmail.com>
2017-11-23 16:39:30 +0100

    Mise a jours des php

--- public/php/connexion.php
+++ public/php/connexion.php
@@ -17,13 +17,13 @@ if(isset($_POST['connexion'])) { // si le bouton "Connexion" est appuyé
             $Pseudo = htmlentities($_POST['pseudo'], ENT_QUOTES, "ISO-8859-1"); // le htmlentities() passera les guillemets en entités HTML, ce qui empêchera les injections SQL
             $MotDePasse = htmlentities($_POST['mot_de_passe'], ENT_QUOTES, "ISO-8859-1");
             //on se connecte à la base de données:
-            $mysqli = mysqli_connect("domaine.tld", "nom d'utilisateur", "mot de passe", "base de données");
+            $mysqli = mysqli_connect("db711052003.db.1and1.com", "dbo711052003", "TP_config27", "db711052003");
             //on vérifie que la connexion s'effectue correctement:
             if(!$mysqli){
                 echo "Erreur de connexion à la base de données.";
             } else {
                 // on fait maintenant la requête dans la base de données pour rechercher si ces données existe et correspondent:
-                $Requete = mysqli_query($mysqli,"SELECT * FROM table_membres WHERE pseudo = '".$Pseudo."' AND mot_de_passe = '".$MotDePasse."'");
+                $Requete = mysqli_query($mysqli,"SELECT * FROM users WHERE pseudo = '".$Pseudo."' AND mdp = '".$MotDePasse."'");
                 // si il y a un résultat, mysqli_num_rows() nous donnera alors 1
                 // si mysqli_num_rows() retourne 0 c'est qu'il a trouvé aucun résultat
                 if(mysqli_num_rows($Requete) == 0) {
